2148f5d6cc7b4ca46e2d399a88eba5ba6579f83e
Jenny Gao <jgao658@gmail.com>
2018-02-08 18:35:13 -0500

    working

--- draw.py
+++ draw.py
@@ -48,7 +48,7 @@ def draw_line( x0, y0, x1, y1, screen, color ):
             d=A-2*B
             while y>=y1:
                 plot(screen,color,x,y)
-                if d<0:
+                if d>0:
                     x+=1
                     d+=2*A
                 y-=1
@@ -59,8 +59,27 @@ def draw_line( x0, y0, x1, y1, screen, color ):
             draw_line(x1,y1,x0,y0,screen,color)
         elif m<0 and m>-1 and x1<x0:  ##Octant 4
             draw_line(x1,y1,x0,y0,screen,color)
-        else:  ##Octant 3
+        elif m<-1 and y1>y0:  ##Octant 3
             draw_line(x1,y1,x0,y0,screen,color)
+        elif m==1:
+            smallX=min(x0,x1)
+            bigX=max(x0,x1)
+            for i in range(smallX,bigX+1):
+                plot(screen,color,i,i)
+        elif m==-1:
+            smallX=min(x0,x1)
+            bigX=max(x0,x1)
+            if smallX==x0:
+                y=y0
+            else:
+                y=y1
+            while smallX<=bigX:
+                plot(screen,color,smallX,y)
+                smallX+=1
+                y-=1
+            #for i in range(smallX,bigX+1):
+                #plot(screen,color,i,i*-1)
+            
             
             
 
--- main.py
+++ main.py
@@ -2,8 +2,33 @@ from display import *
 from draw import *
 
 screen = new_screen()
-color = [ 0, 255, 0 ]
+color = [ 0, 0, 255 ]
+#color = [ 0, 255, 0 ]
 
-#draw_line(250,250,230,270, screen, color)   
+#draw_line(230,320,250,280, screen, color)
+draw_line(250,280,230,320, screen, color)
+draw_line(230,320,210,280, screen, color)   
+draw_line(210,280,160,280, screen, color)
+draw_line(160,280,130,270, screen, color)
+draw_line(110,260,130,270, screen, color)
+draw_line(110,260,140,250, screen, color)
+draw_line(140,250,120,240, screen, color)
+draw_line(120,240,160,230, screen, color)
+draw_line(160,230,170,200, screen, color)
+draw_line(170,200,190,180, screen, color)
+draw_line(190,180,190,230, screen, color)
+draw_line(190,230,210,220, screen, color)
+draw_line(260,220,210,220, screen, color)
+draw_line(260,220,280,210, screen, color)
+draw_line(290,230,280,210, screen, color)
+draw_line(290,230,350,240, screen, color)
+draw_line(380,230,350,240, screen, color)
+draw_line(380,230,370,250, screen, color)
+draw_line(380,280,370,250, screen, color)
+draw_line(380,280,390,300, screen, color)
+draw_line(340,260,390,300, screen, color)
+draw_line(340,260,280,270, screen, color)
+draw_line(250,280,280,270, screen, color)
+
 display(screen)
 save_extension(screen, 'img.png')
