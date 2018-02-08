e7f08a167793ca3e46fc0fa4df26121d65976825
Jenny Gao <jgao658@gmail.com>
2018-02-08 13:57:35 -0500

    error test case

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
-draw_line(0,0,50,70, screen, color)   
+#draw_line(250,250,230,270, screen, color)   
 display(screen)
 save_extension(screen, 'img.png')
